99b41ec3f7ef18aa07a54f6567d851f1668d2530
Gustavo <166073286+maguila-gus25@users.noreply.github.com>
2024-10-14 20:09:40 -0300

    Update clientemodelo.py

--- clientemodelo.py
+++ clientemodelo.py
@@ -6,14 +6,12 @@ class ClienteModelo:
     - nome
     - fone
     - email
-    - historico_compras
   """
   
-    def __init__(self, nome: str, fone: str, email: str, historico_compras: None):
+    def __init__(self, nome: str, fone: str, email: str):
         self.__nome = nome
         self.__fone = fone
         self.__email = email
-        self.__historico_compras = historico_compras
 
     @property
     def nome(self):
