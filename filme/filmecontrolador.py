12a02593018f04b7eb543be1d819ed95a5d0ac3d
Gustavo <166073286+maguila-gus25@users.noreply.github.com>
2024-10-29 20:54:25 -0300

    Update filmecontrolador.py

--- filme/filmecontrolador.py
+++ filme/filmecontrolador.py
@@ -4,7 +4,7 @@ from filme import Filme, ClassificacaoEtaria
 class FilmeControlador:
     filmes_db = []  # Simulação do banco de dados em memória
 
-    def adicionar_filme(self, titulo: str, duracao: str, genero: str, classificacao_etaria: int, sinopse: str):
+    def adicionar_filme(self, titulo: str, duracao: str, genero: str, classificacao_etaria: int):
         # Converte a duração para datetime.time
         try:
             horas, minutos = map(int, duracao.split(":"))
@@ -18,11 +18,11 @@ class FilmeControlador:
                 return f"Filme {titulo} já está cadastrado."
 
         # Cria e adiciona o novo filme
-        novo_filme = Filme(titulo, duracao_time, genero, ClassificacaoEtaria(classificacao_etaria), sinopse)
+        novo_filme = Filme(titulo, duracao_time, genero, ClassificacaoEtaria(classificacao_etaria))
         FilmeControlador.filmes_db.append(novo_filme)
         return f"Filme {titulo} foi adicionado com sucesso!"
 
-    def atualizar_filme(self, titulo: str, duracao: str = None, genero: str = None, classificacao_etaria: int = None, sinopse: str = None):
+    def atualizar_filme(self, titulo: str, duracao: str = None, genero: str = None, classificacao_etaria: int = None):
         # Procura o filme pelo título e, se encontrado, atualiza seus atributos
         for filme in FilmeControlador.filmes_db:
             if filme.titulo == titulo:
@@ -36,8 +36,6 @@ class FilmeControlador:
                     filme.genero = genero
                 if classificacao_etaria is not None:
                     filme.classificacao_etaria = ClassificacaoEtaria(classificacao_etaria)
-                if sinopse is not None:
-                    filme.sinopse = sinopse
                 return f"Filme {titulo} atualizado com sucesso!"
         return f"Filme {titulo} não encontrado."
 
