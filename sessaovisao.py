24a65fc9bac78664839d8706a548758f6e590f33
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-11-03 14:46:10 -0300

    Update sessaovisao.py

--- sessaovisao.py
+++ sessaovisao.py
@@ -1,11 +1,6 @@
-from horarioinvalido import HorarioInvalido
-from sessaocontrolador import SessaoControlador
-from filme import Filme
-from filmevisao import FilmeVisao
-from sala import Sala
-from sessao import Sessao
-from sessao import TipoSessao
-from datetime import time
+from exception.horarioinvalido import HorarioInvalido
+import re
+from entidade.sessao import TipoSessao
 
 class SessaoVisao:
     """
@@ -13,8 +8,8 @@ class SessaoVisao:
     """
 
     def __init__(self):
-        self.controlador = SessaoControlador()  # Associação com o controlador de sessões
-        self.filme = Filme
+        pass
+    # self.controlador = SessaoControlador
 
     def tela_opcoes(self):
         print("\n-- Menu Sessão --")
@@ -34,55 +29,81 @@ class SessaoVisao:
         return opcao
 
     def pega_dados_sessao(self):
-        while True:
-            try:
-                # Entrada e validação do título e duração do filme
-                print("==== ADICIONAR FILME ====")
-                titulo = input("TÍTULO: ")
-                duracao = int(input("DURAÇÃO (em minutos): "))
-                classificacao_etaria = int(input("CLASSIFICAÇÃO ETÃRIA: "))
-                
-                if duracao <= 0:
-                    raise ValueError("A duração deve ser um valor positivo.")
-                filme = Filme(titulo, duracao, classificacao_etaria)
-                
-                # Entrada e validação da sala2
-                print("==== ADICIONAR SALA ====")
-                numero_sala = int(input("Digite o número da sala: "))
-                capacidade = int(input("Digite a capacidade da sala: "))
-                if capacidade <= 0:
-                    raise ValueError("A capacidade deve ser um valor positivo.")
-                sala = Sala(numero_sala, capacidade)
+        try:
+            print("===== ESCOLHA O FILME =====")
+            titulo = input("TÍTULO DO FILME: ")
 
-                # Entrada e validação do horário utilizando validar_horario
-                horario = input("Digite o horário da sessão (HH:MM): ")
-                self.controlador.validar_horario(horario)
+            print("===== ESCOLHA A SALA =====")
+            sala_numero = int(input("NÚMERO DA SALA: "))
 
-                # Escolha do tipo de sessão
-                print("Escolha o tipo de sessão:")
-                for tipo in TipoSessao:
-                    print(f"{tipo.value}. {tipo.name}")
+            print("===== DADOS DA SESSÃO =====")
+            horario = input("Digite o horário da sessão (HH:MM): ")
+            if not re.match(r'^\d{2}:\d{2}$', horario):
+                raise HorarioInvalido(horario)
 
-                tipo_escolhido = int(input("Digite o número correspondente ao tipo de sessão: "))
-                if tipo_escolhido not in [tipo.value for tipo in TipoSessao]:
-                    raise ValueError("Tipo de sessão inválido.")
+                # Verificação adicional para horas e minutos
+            horas, minutos = map(int, horario.split(':'))
+            if not (0 <= horas < 24 and 0 <= minutos < 60):
+                raise HorarioInvalido(horario)
+
+            # Escolha do tipo de sessão
+            print("Escolha o tipo de sessão:")
+            for tipo in TipoSessao:
+                print(f"{tipo.value}. {tipo.name}")
+
+            # tipo_escolhido = int(input("Digite o número correspondente ao tipo de sessão: "))
+            # if tipo_escolhido not in [tipo.value for tipo in TipoSessao]:
+            #     raise ValueError("Tipo de sessão inválido.")
+            tipo_escolhido = int(input("Digite o número correspondente ao tipo de sessão: "))
+            if tipo_escolhido in [tipo.value for tipo in TipoSessao]:
                 tipo = TipoSessao(tipo_escolhido)
+            else:
+                raise ValueError("Tipo de sessão inválido.")
 
-                # Se chegarmos aqui, significa que o horário está disponível
-                resultado = self.controlador.adicionar_sessao(filme, sala, horario, tipo)
-                print(resultado)  # Exibe a mensagem de sucesso
-                return {"filme": filme, "sala": sala, "horario": horario,
-                        "tipo": tipo}
+            return {"titulo": titulo, "sala_numero": sala_numero, "horario": horario, "tipo": tipo}
 
-            except ValueError as ve:
-                print(f"Erro de valor: {ve}. Tente novamente.")
-
-            except HorarioInvalido:
-                print("Horário inválido. Use o formato HH:MM.")
+        except ValueError as ve:
+            print(f"Erro de valor: {ve}. Tente novamente.")
+        except HorarioInvalido as hi:
+            print(hi)
 
     # def pega_dados_sessao(self):
-    #     filme = input("Digite o nome do filme: ")
-    #     sala = int(input("Digite o número da sala: "))
+    #     try:
+    #         print("===== ESCOLHA O FILME =====")
+    #         titulo = input("TÍTULO DO FILME: ")
+    #
+    #         print("===== ESCOLHA A SALA =====")
+    #         sala_numero = int(input("NÚMERO DA SALA: "))
+    #
+    #         print("===== DADOS DA SESSÃO =====")
+    #         horario = input("Digite o horário da sessão (HH:MM): ")
+    #         if not re.match(r'^\d{2}:\d{2}$', horario):
+    #             raise HorarioInvalido(horario)
+    #
+    #             # Verificação adicional para horas e minutos
+    #         horas, minutos = map(int, horario.split(':'))
+    #         if not (0 <= horas < 24 and 0 <= minutos < 60):
+    #             raise HorarioInvalido(horario)
+    #
+    #         # Escolha do tipo de sessão
+    #         print("Escolha o tipo de sessão:")
+    #         for tipo in TipoSessao:
+    #             print(f"{tipo.value}. {tipo.name}")
+    #
+    #         tipo_escolhido = int(input("Digite o número correspondente ao tipo de sessão: "))
+    #         if tipo_escolhido not in [tipo.value for tipo in TipoSessao]:
+    #             raise ValueError("Tipo de sessão inválido.")
+    #
+    #         return {"titulo": titulo, "sala_numero": sala_numero, "horario": horario, "tipo": tipo}
+    #
+    #     except ValueError as ve:
+    #         print(f"Erro de valor: {ve}. Tente novamente.")
+    #     except HorarioInvalido as hi:
+    #         print(hi)
+
+    # def pega_dados_sessao(self):
+    #     controlador = input("Digite o nome do controlador: ")
+    #     visao = int(input("Digite o número da visao: "))
     #     horario = input("Digite o horário da sessão (HH:MM): ")
     #     ingressos_disponiveis = int(input("Digite a quantidade de ingressos disponíveis: "))
     #
@@ -95,7 +116,7 @@ class SessaoVisao:
     #         tipo_escolhido = int(input("Digite o número correspondente ao tipo de sessão: "))
     #         tipo = TipoSessao(tipo_escolhido)  # Converte a escolha para o tipo Enum
     #
-    #         return {"filme": filme, "sala": sala, "horario": horario,
+    #         return {"controlador": controlador, "visao": visao, "horario": horario,
     #                 "ingressos_disponiveis": ingressos_disponiveis, "tipo": tipo}
     #     except ValueError as e:
     #         print(f"Erro: {e}. Tente novamente.")
@@ -107,29 +128,39 @@ class SessaoVisao:
     def mostra_mensagem(self, mensagem):
         print(mensagem)
 
-    def exibe_lista_sessoes(self, sessoes_db):
-        """
-        Exibe a lista de sessões cadastradas.
+    def mostra_sessao(self, dados_sessao):
+        print("\n")
+        print("TÍTULO: ", dados_sessao["titulo"])
+        print("SALA: ", dados_sessao["numero_sala"])
+        print("HORARIO: ", dados_sessao["horario"])
+        print("TIPO: ", dados_sessao["tipo"].name)
+        print("INGRESSOS DISPONIVEIS: ", dados_sessao["ingressos_disponiveis"])
+        print("\n")
 
-        :param lista_sessoes: Lista de sessões a serem exibidas.
-        """
-        if isinstance(sessoes_db, str):
-            self.mostra_mensagem(sessoes_db)  # Exibe mensagem de erro, se for uma string
-        else:
-            if not sessoes_db:  # Verifica se a lista de sessões está vazia
-                self.mostra_mensagem("Nenhuma sessão cadastrada.")
-                return
+    # def exibe_lista_sessoes(self, sessoes_db):
+    #     """
+    #     Exibe a lista de sessões cadastradas.
+    #
+    #     :param lista_sessoes: Lista de sessões a serem exibidas.
+    #     """
+    #     if isinstance(sessoes_db, str):
+    #         self.mostra_mensagem(sessoes_db)  # Exibe mensagem de erro, se for uma string
+    #     else:
+    #         if not sessoes_db:  # Verifica se a lista de sessões está vazia
+    #             self.mostra_mensagem("Nenhuma sessão cadastrada.")
+    #             return
+    #
+    #         print("\n-- Lista de Sessões --")
+    #         for sessao in sessoes_db:
+    #             # Acessa os atributos da sessão e imprime suas informações
+    #             print(f"Filme: {sessao.filme.titulo}, "
+    #                   f"Sala: {sessao.sala.numero}, "
+    #                   f"Horário: {sessao.horario}, "
+    #                   f"Capacidade: {sessao.sala.capacidade}, "
+    #                   f"Tipo: {sessao.tipo.name}")
 
-            print("\n-- Lista de Sessões --")
-            for sessao in sessoes_db:
-                # Acessa os atributos da sessão e imprime suas informações
-                print(f"Filme: {sessao.filme.titulo},\n "
-                      f"Sala: {sessao.sala.numero},\n "
-                      f"Horário: {sessao.horario},\n "
-                      f"Capacidade: {sessao.sala.capacidade},\n "
-                      f"Tipo: {sessao.tipo.name}\n")
 
-    def exibe_lista_ingressos(self, ingressos):
+    def mostra_ingressos(self, ingressos):
         """
         Exibe a lista de ingressos vendidos.
 
@@ -144,17 +175,17 @@ class SessaoVisao:
 
             print("\nIngressos vendidos:")
             for ingresso in ingressos:
-                # Assume que o ingresso possui atributos 'sessao' e 'cliente'
+                # Assume que o entidade possui atributos 'sessao' e 'cliente'
                 print(f"Filme: {ingresso.sessao.filme.titulo}, "
                       f"Sala: {ingresso.sessao.sala}, "
                       f"Horário: {ingresso.sessao.horario}, "
                       f"Cliente: {ingresso.cliente.nome}")
 
     def seleciona_sessao(self):
-        filme = input("Digite o nome do filme da sessão: ")
-        sala = int(input("Digite o número da sala: "))
+        titulo = input("Digite o nome do titulo da sessão: ")
+        sala_numero = int(input("Digite o número da sala: "))
         horario = input("Digite o horário da sessão (HH:MM): ")
-        return {"filme": filme, "sala": sala, "horario": horario}
+        return {"titulo": titulo, "sala_numero": sala_numero, "horario": horario}
 
     def pega_novos_dados_sessao(self):
         """
