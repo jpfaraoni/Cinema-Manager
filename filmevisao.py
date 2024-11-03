fccc8ea4ad3f8e2bb6006cb1427557f562735e91
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-11-03 14:56:26 -0300

    Update filmevisao.py

--- filmevisao.py
+++ filmevisao.py
@@ -1,13 +1,9 @@
-from filmecontrolador import FilmeControlador
-from datetime import time
-
 class FilmeVisao:
     def __init__(self):
-        self.controlador = FilmeControlador()  # Instancia do controlador
+        pass
 
     def tela_opcoes(self):
         print("-------- GERENCIAMENTO DE FILMES ----------")
-        print("Escolha a opção:")
         print("1 - Adicionar Filme")
         print("2 - Atualizar Filme")
         print("3 - Remover Filme")
@@ -19,67 +15,59 @@ class FilmeVisao:
             if opcao not in [0, 1, 2, 3, 4]:
                 raise ValueError("Opção inválida.")
         except ValueError as e:
-            print(f"Erro: {e}. Por favor, digite um número válido.")
-            return self.tela_opcoes()  # Chama novamente para uma entrada correta
+            print(f"Erro: {e}")
+            return self.tela_opcoes()
 
         return opcao
 
-    from datetime import time
-
     def pega_dados_filme(self):
-        """
-        Solicita os dados de um filme ao usuário e retorna um dicionário com esses dados.
-        """
         print("-------- DADOS DO FILME ----------")
-
-        titulo = input("Título do filme: ")
-        
-        # Solicita a duração no formato HH:MM
-        duracao_str = input("Duração do filme (HH:MM): ")
         try:
-            horas, minutos = map(int, duracao_str.split(":"))
-            duracao = time(horas, minutos)
+            titulo = input("Título do filme: ")
+
+            duracao = int(input("Duração do filme(em minutos): "))
+
+            genero = input("Digite o genero: ")
+
+            classificacao_etaria = int(input("Classificação etária do filme: "))
+
+            return {
+                "titulo": titulo,
+                "duracao": duracao,
+                "genero": genero,
+                "classificacao_etaria": classificacao_etaria
+            }
         except ValueError:
-            raise ValueError("Formato de duração inválido. Use HH:MM.")
-        classificacao_etaria = int(input("Classificação etária do filme: "))
-        
-        return {
-            "titulo": titulo,
-            "duracao": duracao,
-            "classificacao_etaria": classificacao_etaria
-        }
+            raise ValueError("Dados inválidos.")
 
-
-    def mostra_filme(self, dados_filme):
-        print("-------- FILME ----------")
-        print(f"TÍTULO: {dados_filme.titulo}")
-        print(f"DURAÇÃO: {dados_filme.duracao}")
-        print(f"CLASSIFICAÇÃO: {dados_filme.classificacao_etaria}")
-        print("\n")
-
-    def seleciona_filme(self):
+    def pega_novos_dados_filme(self):
         try:
-            titulo = input("Digite o título do filme que deseja selecionar: ")
-        except ValueError:
-            print("Erro: título inválido. Por favor, insira um título válido.")
-            return self.seleciona_filme()  # Chama novamente para uma entrada correta
+            duracao = int(input("Nova duração do filme(em minutos): "))
 
-        return titulo
+            genero = input("Novo gênero do filme: ")
+
+            classificacao_etaria = int(input("Nova classificação etária do filme: "))
+
+            return {
+                "duracao": duracao,
+                "genero": genero,
+                "classificacao_etaria": classificacao_etaria
+            }
+        except ValueError:
+            raise ValueError("Dados inválidos.")
 
     def mostra_mensagem(self, msg):
-        """
-        Exibe uma mensagem para o usuário.
-        """
         print(msg)
 
-    def listar_filmes(self, filmes):
-        if isinstance(filmes, str):  
-            self.mostra_mensagem(filmes)
-        else:
-            if not filmes:  # Verifica se a lista de salas está vazia
-                self.mostra_mensagem("Nenhum filme cadastrado.")
-                return
+    def seleciona_filme(self):
+        titulo = input("Digite o título do filme que deseja selecionar: ")
+        return titulo
 
-            print("\nFilmes cadastrados:")
-            for filme in filmes:
-                self.mostra_filme(filme)
+    def listar_filmes(self, filmes):
+        if not filmes:
+            self.mostra_mensagem("Nenhum filme cadastrado.")
+            return
+
+        print("\nFilmes cadastrados:")
+        for filme in filmes:
+            print(f"TÍTULO: {filme['titulo']}, DURAÇÃO: {filme['duracao']}, GÊNERO: {filme['genero']}, CLASSIFICAÇÃO: {filme['classificacao_etaria']}")
