6bc075126637e9c4239d0c9bbf5a4a0e2e173811
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-11-03 15:17:10 -0300

    Update and rename sistema_cinema.py to sistemacontrolador.py

---
+++ sistemacontrolador.py
@@ -0,0 +1,51 @@
+from visao.sistemavisao import SistemaVisao
+from controlador.filmecontrolador import FilmeControlador
+from controlador.salacontrolador import SalaControlador
+from controlador.sessaocontrolador import SessaoControlador
+
+
+class SistemaControlador:
+
+    def __init__(self):
+        self.__filmecontrolador = FilmeControlador(self)
+        self.__salacontrolador = SalaControlador(self)
+        self.__sessaocontrolador = SessaoControlador(self)
+        self.__sistemavisao = SistemaVisao()
+
+    @property
+    def filmecontrolador(self):
+        return self.__filmecontrolador
+
+    @property
+    def salacontrolador(self):
+        return self.__salacontrolador
+
+    @property
+    def sessaocontrolador(self):
+        return self.__sessaocontrolador
+
+    def inicializa_sistema(self):
+        self.abre_tela()
+
+    def cadastra_filme(self):
+        self.__filmecontrolador.abre_tela()
+
+    def cadastra_sessao(self):
+        # Chama o controlador de Amigos
+        self.__sessaocontrolador.abre_tela()
+
+    def cadastra_sala(self):
+        self.__salacontrolador.abre_tela()
+        # Chama o controlador de Emprestimos
+
+    def encerra_sistema(self):
+        exit(0)
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.cadastra_sala, 2: self.cadastra_filme, 3: self.cadastra_sessao,
+                        0: self.encerra_sistema}
+
+        while True:
+            opcao_escolhida = self.__sistemavisao.tela_opcoes()
+            funcao_escolhida = lista_opcoes[opcao_escolhida]
+            funcao_escolhida()
