61795015496e48ae36eb24df1a7ba08259cad21b
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-11-03 21:42:06 -0300

    Update sistemavisao.py

--- visao/sistemavisao.py
+++ visao/sistemavisao.py
@@ -4,15 +4,16 @@ class SistemaVisao:
 
     def tela_opcoes(self):
         print("--------  ---------")
-        print("+++++ ESCOLHA SUA OPÇÃO +++++")
-        print("1 - Cadastro de Sala: ")
-        print("2 - Cadastro de Filme: ")
-        print("3 - Adicionar Sessão: ")
+        print("===== ESCOLHA SUA OPÇÃO =====")
+        print("1 - Cadastro de Sala ")
+        print("2 - Cadastro de Filme ")
+        print("3 - Adicionar Sessão ")
+        print("4 - Visualizar Relatório de Turnos Mais Frequentados")
         print("0 - Finalizar sistema")
 
         try:
             opcao = int(input("Escolha a opção: "))
-            if opcao not in [0, 1, 2, 3]:
+            if opcao not in [0, 1, 2, 3, 4]:
                 raise ValueError("Opção inválida.")
         except ValueError as e:
             print(f"Erro: {e}")
