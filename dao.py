ff7db5efb534191dbd652ff9b33f9ea9afeea811
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-12-08 18:18:37 -0300

    Create dao.py

---
+++ dao.py
@@ -0,0 +1,46 @@
+import pickle
+from abc import ABC, abstractmethod
+
+# Classe DAO abstrata
+class DAO(ABC):
+    def __init__(self, datasource= ''):
+        self.__datasource = datasource
+        self.__cache = {}
+        try:
+            self.__load()
+        except FileNotFoundError:
+            self.__dump()
+
+    def __dump(self):
+        pickle.dump(self.__cache, open(self.__datasource, 'wb'))
+
+    def __load(self):
+        self.__cache = pickle.load(open(self.__datasource, 'rb'))
+
+    def add(self, key, obj):
+        self.__cache[key] = obj
+        self.__dump()
+
+    def update(self, key, obj):
+        try:
+            if(self.__cache[key] != None):
+                self.__cache[key] = obj #atualiza a entrada
+                self.__dump()  #atualiza o arquivo
+        except KeyError:
+            pass
+
+    def get(self, key):
+        try:
+            return self.__cache[key]
+        except KeyError:
+            pass
+
+    def remove(self, key):
+        try:
+            self.__cache.pop(key)
+            self.__dump()
+        except KeyError:
+            pass
+
+    def get_all(self):
+        return self.__cache.values()
