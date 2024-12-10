dd627fdda14dd834b82b7bbdfe13b694552b1775
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-12-10 12:01:28 -0300

    Update sala_visao.py

--- visao/sala_visao.py
+++ visao/sala_visao.py
@@ -1,12 +1,16 @@
 import PySimpleGUI as sg
+from PIL import Image
 
 class SalaVisao:
     def __init__(self):
         pass
 
+    # Image.open("menus_sala.jpg").resize((320, 60)).save("menus_sala_red.jpg")
     def tela_opcoes(self):
+        sg.ChangeLookAndFeel('DarkGrey10')
         layout = [
-            [sg.Text("-------- GERENCIAMENTO DE SALAS ----------", size=(30, 1))],
+            # [sg.Image(filename='menus_sala_red.jpg')],
+            [sg.Text("GERENCIAMENTO DE SALAS", size=(30, 1))],
             [sg.Button("Adicionar Sala", key=1)],
             [sg.Button("Atualizar Sala", key=2)],
             [sg.Button("Remover Sala", key=3)],
@@ -20,6 +24,7 @@ class SalaVisao:
         return event
 
     def pega_dados_sala(self):
+        sg.ChangeLookAndFeel('DarkGrey10')
         layout = [
             [sg.Text("Número da sala:"), sg.InputText(key="numero")],
             [sg.Text("Capacidade da sala:"), sg.InputText(key="capacidade")],
@@ -43,6 +48,7 @@ class SalaVisao:
             return None
 
     def pega_novos_dados_sala(self):
+        sg.ChangeLookAndFeel('DarkGrey10')
         layout = [
             [sg.Text("Nova capacidade da sala:"), sg.InputText(key="capacidade")],
             [sg.Button("Confirmar"), sg.Button("Cancelar")],
@@ -62,6 +68,7 @@ class SalaVisao:
             return None
 
     def seleciona_sala(self):
+        sg.ChangeLookAndFeel('DarkGrey10')
         layout = [
             [sg.Text("Digite o número da sala que deseja selecionar:"), sg.InputText(key="numero")],
             [sg.Button("Confirmar"), sg.Button("Cancelar")],
@@ -81,9 +88,11 @@ class SalaVisao:
             return None
 
     def mostra_mensagem(self, msg):
+        sg.ChangeLookAndFeel('DarkGrey10')
         sg.popup(msg)
 
     def exibe_lista_salas(self, salas):
+        sg.ChangeLookAndFeel('DarkGrey10')
         if not salas:
             sg.popup("Nenhuma sala cadastrada.")
             return
