7549b1b6aff079def6baeb3f5ae9ce248b6cfdac
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-12-12 20:18:54 -0300

    Update sistema_visao.py

--- sistema_visao.py
+++ sistema_visao.py
@@ -1,24 +1,41 @@
+import PySimpleGUI as sg
+
 class SistemaVisao:
     def __init__(self):
         pass
 
     def tela_opcoes(self):
-        print("-------- SISTEMA ---------")
-        print("===== ESCOLHA SUA OPÇÃO =====")
-        print("1 - Cadastro de Sala ")
-        print("2 - Cadastro de Filme ")
-        print("3 - Adicionar Sessão ")
-        print("4 - Realizar Venda")
-        print("5 - Cadastro de Cliente")
-        print("6 - Relatório Turno Com Mais Sessões")
-        print("0 - Finalizar sistema")
+        """
+        Exibe a interface gráfica do menu principal do sistema.
 
-        try:
-            opcao = int(input("Escolha a opção: "))
-            if opcao not in [0, 1, 2, 3, 4, 5, 6]:
-                raise ValueError("Opção inválida.")
-        except ValueError as e:
-            print(f"Erro: {e}")
-            return self.tela_opcoes()
+        Retorna:
+            int: Código da opção selecionada pelo usuário.
+        """
+        sg.ChangeLookAndFeel('DarkGrey10')
+        layout_esquerda = [
+            [sg.Image(filename='visao/imagens/rb_13067.png')]
+        ]
+        layout_direita = [
+            [sg.Button("Salas", key=1, size=(9, 1), font=("Helvetica", 12))],
+            [sg.Button("Filmes", key=2, size=(9, 1), font=("Helvetica", 12))],
+            [sg.Button("Sessões", key=3, size=(9, 1), font=("Helvetica", 12))],
+            [sg.Button("Realizar Venda", key=4, size=(9, 1), font=("Helvetica", 12))],
+            [sg.Button("Cadastro Cliente", key=5, size=(9, 1), font=("Helvetica", 12))],
+            [sg.Button("Relatório", key=6, size=(9, 1), font=("Helvetica", 12))],
+            [sg.Button("Finalizar Sistema", size=(9, 1), font=("Helvetica", 12))],
+        ]
 
-        return opcao
+        layout = [
+            [sg.Column(layout_direita),
+            sg.VSeparator(),
+            sg.Column(layout_esquerda)]
+            ]
+        # Cria a janela com o layout definido
+        window = sg.Window("Menu Principal", layout, finalize=True)
+
+        # Lê o evento do botão pressionado
+        event, _ = window.read()
+        window.close()
+
+        # Retorna o código da opção selecionada
+        return event if isinstance(event, int) else 0
