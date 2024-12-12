714b94748f019c71f55550c6e40664e27072a94e
jpfaraoni <120076832+jpfaraoni@users.noreply.github.com>
2024-12-12 20:40:24 -0300

    Update sessao_nao_encontrada.py

--- exception/sessao_nao_encontrada.py
+++ exception/sessao_nao_encontrada.py
@@ -1,3 +1,3 @@
 class SessaoNaoEncontrada(Exception):
-    def __init__(self, titulo_filme, sala, horario):
-        super().__init__(f"Sessão não encontrada para o filme '{titulo_filme}', sala {sala}, horário {horario}.")
+    def __init__(self, codigo: int):
+        super().__init__(f"Sessão não encontrada para o código {codigo}")
